b3431db3102dd5c81e0414dd44496094e1d24b5d
Alexis Lucattini <alexis.lucattini@umccr.org>
2022-10-23 12:38:19 +1100

    Added in icav2 commands
    * icav2-zip-workflow
    * icav2-deploy-pipeline
    * icav2-launch-pipeline-analysis

---
+++ subcommands/v2/deploy_to_v2.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+"""
+Deploy a zipped workflow to icav2
+
+Provide a default storage size and a project-name / project id and you're done!
+
+Return a pipeline ID
+"""
+
+import os
+from utils.icav2_helpers import get_project_id_from_project_name, \
+    get_icav2_configuration, is_project_id_format, \
+    get_analysis_storage_id_from_analysis_storage_size, create_workflow_from_zip_path
+
+from classes.command import Command
+from utils.logging import get_logger
+from pathlib import Path
+from argparse import ArgumentError
+from utils.globals import ICAv2AnalysisStorageSize, ICAV2_DEFAULT_ANALYSIS_STORAGE_SIZE
+from utils.errors import CheckArgumentError
+from typing import Optional
+
+logger = get_logger()
+
+
+class DeployV2Workflow(Command):
+    """Usage:
+    cwl-ica [options] icav2-deploy-pipeline help
+    cwl-ica [options] icav2-deploy-pipeline (--zipped-workflow-path=<zipped-workflow-path>)
+                                            (--project-name=<project_name> | --project-id=<project_id>)
+                                            [--analysis-storage-id=<analysis_storage_id> | --analysis-storage-size=<analysis_storage_size>]
+
+Description:
+    From a zip file, deploy a workflow to icav2
+
+Options:
+    --zipped-workflow-path=<workflow path>           Required, path to zipped up workflow
+    --project-id=<project_id>                        Optional, provide the project id (takes precedence over --project-name)
+    --project-name=<project_name>                    Optional, provide the project name
+    --analysis-storage-id=<analysis_storage_id>      Optional, takes precedence over analysis-storage-size
+    --analysis-storage-size=<analysis_storage_size>  Optional, default is set to Small
+
+Environment:
+    ICAV2_ACCESS_TOKEN
+
+Example:
+    cwl-ica icav2-deploy-pipeline --zipped-workflow-path bclconvert-with-qc-pipeline__4.0.3.zip --project-name playground_v2
+    """
+
+    def __init__(self, command_argv):
+
+        # Collect args from doc strings
+        super(DeployV2Workflow, self).__init__(command_argv)
+
+        # Initialise parameters
+        self.zipped_workflow_path: Optional[Path] = None
+        self.project_id: Optional[str] = None
+        self.project_name: Optional[str] = None
+        self.analysis_storage_size: Optional[ICAv2AnalysisStorageSize] = None
+        self.analysis_storage_id: Optional[str] = None
+        self.code: Optional[str] = None
+        self.pipeline_id: Optional[str] = None
+
+        # Check if help has been called
+        if self.args["help"]:
+            self._help()
+
+        # Confirm 'required' arguments are present and valid
+        try:
+            logger.debug("Checking args")
+            self.check_args()
+        except ArgumentError:
+            self._help(fail=True)
+
+    def check_args(self):
+        # Confirm zipped_workflow_path is set and is a file
+        zipped_workflow_path_arg = self.args.get("--zipped-workflow-path", None)
+        if zipped_workflow_path_arg is None:
+            logger.error("--zipped-workflow-path not defined")
+            raise CheckArgumentError
+        self.zipped_workflow_path = Path(zipped_workflow_path_arg)
+        if not self.zipped_workflow_path.is_file():
+            logger.error("Could not find --zipped-workflow-path")
+            raise FileNotFoundError
+
+        if os.environ.get("ICAV2_ACCESS_TOKEN", None) is None:
+            logger.error("Could not get env var ICAV2_ACCESS_TOKEN")
+            raise EnvironmentError
+        # TODO check token expiry
+
+        # Check either project name or project id is set
+        project_id = self.args.get("--project-id", None)
+        project_name = self.args.get("--project-name", None)
+
+        if project_id is not None:
+            self.project_id = project_id
+            # Check project id is uuid format
+            if not is_project_id_format(self.project_id):
+                logger.error(f"--project-id parameter {self.project_id} is not of UUID format")
+            # TODO - check is real project
+        elif project_name is not None:
+            self.project_name = self.project_name
+            self.project_id = get_project_id_from_project_name(
+                project_name,
+                get_icav2_configuration()
+            )
+        else:
+            logger.error("Must specify one of --project-id or --project-name")
+            raise CheckArgumentError
+
+        # Get analysis storage size
+        self.analysis_storage_id = self.args.get("--analysis-storage-id", None)
+        self.analysis_storage_size = self.args.get("--analysis-storage-size", None)
+
+        # --analysis-storage-id takes preference
+        if self.analysis_storage_id is not None:
+            pass
+        # --analysis-storage-id is not set and --analysis-storage-size is also not set
+        # Get the default size (SMALL) and take to next step
+        elif self.analysis_storage_size is None:
+            self.analysis_storage_size = ICAV2_DEFAULT_ANALYSIS_STORAGE_SIZE
+        elif self.analysis_storage_size is not None:
+            self.analysis_storage_size = ICAv2AnalysisStorageSize(self.analysis_storage_size)
+        # Use --analysis-storage-size or default value to get --analysis-storage-id
+        if self.analysis_storage_id is None:
+            self.analysis_storage_id = get_analysis_storage_id_from_analysis_storage_size(
+                self.analysis_storage_size,
+                get_icav2_configuration()
+            )
+
+    def __call__(self):
+        self.deploy_workflow()
+
+    def deploy_workflow(self):
+        pipeline_id, code = create_workflow_from_zip_path(
+            self.zipped_workflow_path,
+            self.project_id,
+            self.analysis_storage_id,
+            get_icav2_configuration()
+        )
+
+        logger.info(f"Created pipeline with code '{code}' and id '{pipeline_id}'")
---
+++ subcommands/v2/zip_v2_workflow.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+
+"""
+Given a path to a workflow, create a zip of the workflow ready for deployment to icav2
+
+This involves:
+
+1. Collecting all files related to the workflow (non-trivial step).
+
+1a. Remove any references to hiCpu or hiMem and replace with hicpu or himem respectively
+
+2. Confirm no file is not git-stashed, so we can use the previous commit id in the pipeline reference :construction:
+
+3. Add in a blank params xml step (will be updated once https://jira.illumina.com/browse/SET-3410 is resolved)
+
+4. Create a html document (from cwl auto-generated markdown)  :construction: to add to v2 upload
+"""
+import os
+import shutil
+from fileinput import FileInput
+from zipfile import ZipFile, ZIP_DEFLATED
+
+from classes.command import Command
+from utils.logging import get_logger
+from pathlib import Path
+from argparse import ArgumentError
+from utils.globals import ICAV2_COMPUTE_RESOURCE_MAPPINGS, ICAV2_CONTAINER_MAPPINGS, PARAMS_XML_FILE_NAME, \
+    BLANK_PARAMS_XML_V2_FILE_CONTENTS
+from utils.errors import CheckArgumentError
+from typing import Optional, List, Dict
+from classes.cwl_workflow import CWLWorkflow
+from utils.repo import get_workflows_dir, get_cwl_ica_repo_path
+from utils.miscell import get_name_version_tuple_from_cwl_file_path
+from utils.cwl_workflow_helper_utils import get_step_mappings, collect_objects_recursively
+from utils.cwl_schema_helper_utils import get_schemas
+from utils.cwl_schema_helper_utils import get_schema_mappings
+
+logger = get_logger()
+
+
+class ZipV2Workflow(Command):
+    """Usage:
+    cwl-ica [options] icav2-zip-workflow help
+    cwl-ica [options] icav2-zip-workflow (--workflow-path=<workflow_path>)
+                                         [--output-path=<output_dir_path>]
+                                         [--force]
+
+Description:
+    Create a zip file containing a workflow and all relevant input files to the workflow.
+
+Options:
+    --workflow-path=<workflow path>    Required, path to workflow
+    --output-path=<output_dir_path>    Optional, set the output path, otherwise just the working directory
+    --force                            Optional, override existing zip file if one already exists
+
+Example:
+    cwl-ica icav2-zip-workflow --workflow-path workflows/bclconvert-with-qc-pipeline/4.0.3/bclconvert-with-qc-pipeline__4.0.3.cwl
+    """
+
+    def __init__(self, command_argv):
+
+        # Collect args from doc strings
+        super(ZipV2Workflow, self).__init__(command_argv)
+
+        # Initialise parameters
+        self.cwl_file_path: Optional[Path] = None
+        self.name: Optional[str] = None
+        self.version: Optional[str] = None
+        self.cwl_workflow_obj: Optional[CWLWorkflow] = None
+        self.force: Optional[bool] = None
+        self.output_dir_path: Optional[Path] = None
+        self.output_zip_file: Optional[Path] = None
+
+        # Check if help has been called
+        if self.args["help"]:
+            self._help()
+
+        # Confirm 'required' arguments are present and valid
+        try:
+            logger.debug("Checking args")
+            self.check_args()
+        except ArgumentError:
+            self._help(fail=True)
+
+        # Pull in workflow
+        self.cwl_workflow_obj = CWLWorkflow(
+            self.name,
+            self.version,
+            self.cwl_file_path
+        )
+
+    def check_args(self):
+        # Check defined and assign properties
+        workflow_path_arg = self.args.get("--workflow-path", None)
+        if workflow_path_arg is None:
+            logger.error("--workflow-path not defined")
+            raise CheckArgumentError
+        self.cwl_file_path = Path(workflow_path_arg)
+
+        # Set name and version
+        self.name, self.version = get_name_version_tuple_from_cwl_file_path(
+            self.cwl_file_path,
+            get_workflows_dir()
+        )
+
+        # Check args
+        if not self.cwl_file_path.is_file():
+            logger.error(f"Could not find the file {self.cwl_file_path}")
+            raise CheckArgumentError
+
+        # Check if force specified
+        if self.args.get("--force", False):
+            self.force = True
+        else:
+            self.force = False
+
+        # Check if output_dir_path is set
+        if self.args.get("--output-path", None) is not None:
+            self.output_dir_path = Path(self.args.get("--output-path")).absolute().resolve()
+            if not self.output_dir_path.is_dir():
+                logger.error(f"Expected --output-path set as {self.output_dir_path}, is expected to exist")
+                raise CheckArgumentError
+        else:
+            self.output_dir_path = Path(os.getcwd()).absolute().resolve()
+
+        self.output_zip_file = self.output_dir_path / f"{self.name}__{self.version}.zip"
+
+        if self.output_zip_file.is_file() and not self.force:
+            logger.error(f"Output file {self.output_zip_file} "
+                         f"exists and --force is not set. "
+                         f"Please specify --force to overwrite zip file")
+            raise CheckArgumentError
+        elif self.output_zip_file.is_file() and self.force:
+            logger.info(f"Output file {self.output_zip_file} currently exists and will be overwritten")
+
+    def __call__(self):
+        self.zip_workflow()
+
+    def get_step_mappings(self) -> List[Dict]:
+        return get_step_mappings(self.cwl_workflow_obj.cwl_obj.steps, self.cwl_file_path)
+
+    def get_schemas(self) -> List[Path]:
+        return get_schemas(self.cwl_workflow_obj)
+
+    def get_schema_mappings(self) -> List[Dict]:
+        return get_schema_mappings(self.get_schemas(), self.cwl_file_path)
+
+    def zip_workflow(self):
+        # Collect all the workflow objects
+        all_workflow_objects = collect_objects_recursively(self.cwl_workflow_obj)
+
+        # Create a temporary directory
+        output_tempdir = self.output_dir_path / self.output_zip_file.stem
+
+        # And we want to make sure it doesn't already exist
+        output_tempdir.mkdir(exist_ok=False)
+
+        # Copy over the workflow objects
+        for cwl_item, cwl_file_list in all_workflow_objects.items():
+
+            # Get the cwl file list
+            for cwl_file in cwl_file_list:
+                # Get the new path
+                new_path = output_tempdir.joinpath(cwl_file.relative_to(get_cwl_ica_repo_path()))
+
+                # Create a directory for this file
+                new_path.parent.mkdir(parents=True, exist_ok=False)
+
+                # And copy over contents of the file
+                shutil.copy2(cwl_file, new_path)
+
+        # Copy over the main workflow
+        new_workflow_path = output_tempdir / "workflow.cwl"
+        shutil.copy2(self.cwl_file_path, new_workflow_path)
+
+        # Edit the main workflow
+        with FileInput(new_workflow_path, inplace=True) as _input:
+            for line in _input:
+                line_strip = line.rstrip()
+                for step_mapping in self.get_step_mappings():
+                    if step_mapping.get("old") in line_strip:
+                        line_strip = line_strip.replace(step_mapping.get("old"), step_mapping.get("new"))
+                for schema_mapping in self.get_schema_mappings():
+                    if schema_mapping.get("old") in line_strip:
+                        line_strip = line_strip.replace(schema_mapping.get("old"), schema_mapping.get("new"))
+                print(line_strip)
+
+        # Iterate through all paths and make sure we convert resource requirements?
+        for path_item in output_tempdir.rglob("*"):
+            # Don't need to deal with directories
+            if not path_item.is_file():
+                continue
+
+            # Deal with https://github.com/umccr-illumina/ica_v2/issues/108
+            with FileInput(path_item, inplace=True) as _input:
+                for line in _input:
+                    line_strip = line.rstrip()
+                    for resource_mapping in ICAV2_COMPUTE_RESOURCE_MAPPINGS:
+                        if resource_mapping.get("v1") in line_strip:
+                            line_strip = line_strip.replace(resource_mapping.get("v1"), resource_mapping.get("v2"))
+                    for container_mapping in ICAV2_CONTAINER_MAPPINGS:
+                        if container_mapping.get("v1") in line_strip:
+                            line_strip = line_strip.replace(container_mapping.get("v1"), container_mapping.get("v2"))
+                    print(line_strip)
+
+        # Place the blank params xml in the output temp directory
+        with open(output_tempdir / PARAMS_XML_FILE_NAME, "w") as params_h:
+            for line in BLANK_PARAMS_XML_V2_FILE_CONTENTS:
+                params_h.write(line + "\n")
+
+        # Check zip file doesn't exist
+        if self.output_zip_file.is_file():
+            os.remove(self.output_zip_file)
+
+        # Create the zipped directory
+        with ZipFile(self.output_zip_file, "w", ZIP_DEFLATED) as zip_file:
+            for entry in output_tempdir.rglob("*"):
+                zip_file.write(entry, Path(output_tempdir.name) / Path(entry.relative_to(output_tempdir)))
+
+        # Remove the directory
+        shutil.rmtree(output_tempdir)
